d5bd729d380443424b35b05a10574ca9f5b2c1b5
Nasution <nasutionscience@gmail.com>
2018-12-14 19:58:03 +0700

    home
    [NAS]

---
+++ application/views/Home/index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Home</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link rel="stylesheet" href="<?php echo base_url()?>assets/nas/css/main.css"">
+	</head>
+	<body class="is-preload">
+
+		<!-- Header -->
+			<header id="header">
+				<a class="logo" href="index.html">Donate</a>
+				<nav>
+					<a href="#menu">Menu</a>
+				</nav>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="<?php echo site_url('home/index')?>">Home</a></li>
+					<li><a href="<?php echo site_url('donasi/index')?>">Donasi</a></li>
+					<li><a href="<?php echo site_url('about/index')?>">About Us</a></li>
+				</ul>
+			</nav>
+
+		<!-- Banner -->
+			<section id="banner">
+				<div class="inner">
+					<h1>Donate</h1>
+					<p>Donasikan Hartamu di Dunia<br />
+					 
+						<i>”Seorang mukmin terhadap mukmin lainnya seumpama bangunan saling mengokohkan satu dengan yang lain. (Kemudian Rasulullah SAW merapatkan jari-jari tangan beliau).” (HR. Muttafaq Alaih)</i></p>
+				</div>
+				<!-- <video autoplay loop muted playsinline src="images/banner.mp4"></video> -->
+				<!-- <img src="<?php echo base_url()?>assets/img/sawah.png" alt="" style="width="1263"> -->
+			</section>
+
+		<!-- Highlights -->
+			<section class="wrapper">
+				<div class="inner">
+					<header class="special">
+						<h2>Keunggulan Perusahaan Kami</h2>
+						<p>In arcu accumsan arcu adipiscing accumsan orci ac. Felis id enim aliquet. Accumsan ac integer lobortis commodo ornare aliquet accumsan erat tempus amet porttitor.</p>
+					</header>
+					<div class="highlights">
+						<section>
+							<div class="content">
+								<header>
+									<a href="#" class="icon fa-vcard-o"><span class="label">Icon</span></a>
+									<h3>Feugiat consequat</h3>
+								</header>
+								<p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="#" class="icon fa-files-o"><span class="label">Icon</span></a>
+									<h3>Ante sem integer</h3>
+								</header>
+								<p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="#" class="icon fa-floppy-o"><span class="label">Icon</span></a>
+									<h3>Ipsum consequat</h3>
+								</header>
+								<p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="#" class="icon fa-line-chart"><span class="label">Icon</span></a>
+									<h3>Interdum gravida</h3>
+								</header>
+								<p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="#" class="icon fa-paper-plane-o"><span class="label">Icon</span></a>
+									<h3>Distribusi Cepat</h3>
+								</header>
+								<p>Perusahaan kami memproses pengiriman barang dengan cepat setelah donatur mengirimkan donasinya</p>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<header>
+									<a href="#" class="icon fa-qrcode"><span class="label">Icon</span></a>
+									<h3>Accumsan viverra</h3>
+								</header>
+								<p>Nunc lacinia ante nunc ac lobortis ipsum. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus.</p>
+							</div>
+						</section>
+					</div>
+				</div>
+			</section>
+
+		<!-- CTA -->
+			<section id="cta" class="wrapper">
+				<div class="inner">
+					<h2>Curabitur ullamcorper ultricies</h2>
+					<p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing. Lorem ipsum dolor vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing sed feugiat eu faucibus. Integer ac sed amet praesent. Nunc lacinia ante nunc ac gravida.</p>
+				</div>
+			</section>
+
+		<!-- Testimonials -->
+			<section class="wrapper">
+				<div class="inner">
+					<header class="special">
+						<h2>About US</h2>
+						<p>Donate Merupakan sebuah website yang membantu masyarakat dalam memberi bantuan atau Donasi kepada masyarakat lainya yang sedang terkena musibah</p>
+					</header>
+					<div class="testimonials"> 
+						<section>
+							<div class="content">
+								<blockquote>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur consequatur repellendus distinctio, itaque molestias est! At, sequi, qui. Repudiandae, natus?</p>
+								</blockquote>
+								<div class="author">
+									<div class="image">
+										<img src="<?php echo base_url()?>assets/img/nas.jpg" alt="" />
+									</div>
+									<p class="credit">- <strong>Syamil</strong> <span>Programmer - MosDev Inc.</span></p>
+								</div>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<blockquote>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum vel nam deserunt voluptatum fuga sunt non, ipsa nemo sint saepe..</p>
+								</blockquote>
+								<div class="author">
+									<div class="image">
+										<img src="<?php echo base_url()?>assets/img/nas.jpg" alt="" />
+									</div>
+									<p class="credit">- <strong>Nasution</strong> <span>Programmer - MosDev Inc.</span></p>
+								</div>
+							</div>
+						</section>
+						<section>
+							<div class="content">
+								<blockquote>
+									<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum dolor nam, repudiandae esse porro enim dignissimos modi vitae eveniet doloribus.</p>
+								</blockquote>
+								<div class="author">
+									<div class="image">
+										<img src="<?php echo base_url()?>assets/img/nas.jpg" alt="" />
+									</div>
+									<p class="credit">- <strong>Alif</strong> <span>Programmer - MosDev Inc.</span></p>
+								</div>
+							</div>
+						</section>
+					</div>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="inner">
+					<div class="content">
+						<section>
+							<h3>Accumsan montes viverra</h3>
+							<p>Nunc lacinia ante nunc ac lobortis. Interdum adipiscing gravida odio porttitor sem non mi integer non faucibus ornare mi ut ante amet placerat aliquet. Volutpat eu sed ante lacinia sapien lorem accumsan varius montes viverra nibh in adipiscing. Lorem ipsum dolor vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis volutpat ac adipiscing sed feugiat eu faucibus. Integer ac sed amet praesent. Nunc lacinia ante nunc ac gravida.</p>
+						</section>
+						<section>
+							<h4>Sem turpis amet semper</h4>
+							<ul class="alt">
+								<li><a href="#">Dolor pulvinar sed etiam.</a></li>
+								<li><a href="#">Etiam vel lorem sed amet.</a></li>
+								<li><a href="#">Felis enim feugiat viverra.</a></li>
+								<li><a href="#">Dolor pulvinar magna etiam.</a></li>
+							</ul>
+						</section>
+						<section>
+							<h4>Media Sosial Kami</h4>
+							<ul class="plain">
+								<li><a href="#"><i class="icon fa-twitter">&nbsp;</i>Twitter</a></li>
+								<li><a href="#"><i class="icon fa-facebook">&nbsp;</i>Facebook</a></li>
+								<li><a href="#"><i class="icon fa-instagram">&nbsp;</i>Instagram</a></li>
+								<li><a href="#"><i class="icon fa-github">&nbsp;</i>Github</a></li>
+							</ul>
+						</section>
+					</div>
+					<div class="copyright">
+						<p>&copy; MosDev 2018</p>
+					</div>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="<?php echo base_url()?>assets/nas/js/jquery.min.js"></script>
+			<script src="<?php echo base_url()?>assets/nas/js/browser.min.js"></script>
+			<script src="<?php echo base_url()?>assets/nas/js/breakpoints.min.js"></script>
+			<script src="<?php echo base_url()?>assets/nas/js/util.js"></script>
+			<script src="<?php echo base_url()?>assets/nas/js/main.js"></script>
+
+	</body>
+</html>
